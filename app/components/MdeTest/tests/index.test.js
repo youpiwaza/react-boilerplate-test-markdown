a8d9c942bb05bb8e5da10253fd1726c4362a3112
youpiwaza <masamune.code@gmail.com>
2018-08-13 10:05:27 +0200

    MOD: add rights to prevent cancer

---
+++ app/components/MdeTest/tests/index.test.js
@@ -0,0 +1,10 @@
+// import React from 'react';
+// import { shallow } from 'enzyme';
+
+// import MdeTest from '../index';
+
+describe('<MdeTest />', () => {
+  it('Expect to have unit tests specified', () => {
+    expect(true).toEqual(false);
+  });
+});
